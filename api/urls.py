7d5bce607d83f7f01cc8dd8ea6f9d3c23be64535
achibhossengit <mail.achibhossen@gmail.com>
2025-07-27 20:22:53 +0600

    add JWT related view in url

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,12 @@ from users.views import EmployeeViewSet, PromotionViewSet
 from site_profiles.views import SiteViewSet, SiteCostViewSet, SiteCashViewSet, SiteBillViewSet
 from daily_records.views import DailyRecordViewSet, WorkSessionViewSet, CurrentWorkSession
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
 router = DefaultRouter()
 router.register('employees', EmployeeViewSet, basename='employees')
 router.register('sites', SiteViewSet, basename='sites')
@@ -21,4 +27,8 @@ urlpatterns = [
     path('', include(router.urls)),
     path('', include(employee_router.urls)),
     path('current-worksession/<int:emp_id>/', CurrentWorkSession.as_view(), name='current-work-session'),
+
+    path('token/create/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
